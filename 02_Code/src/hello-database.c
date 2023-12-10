b44d2abf9aaead8bddbb3bbb6a0ca66dbb25c630
neiltbz <z946a@hotmail.com>
2023-12-10 17:49:48 +0100

    Tidies up if clauses

--- 02_Code/src/hello-database.c
+++ 02_Code/src/hello-database.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 #include <sqlite3.h> 
+//
+// Author: Neil Devlin
+//
 
+// Set some global variables
 sqlite3 *GLOBAL_PTR_DATABASE;
 
+// The location for the word database
 char *GLOBAL_DB_FILE = "../db/hello-database.db";
+
+// The query to be performed
 char *GLOBAL_SQL_QUERY = "select * from T_HELLO";
 
-static int callback(void *data, int argc, char **argv, char **azColName){
+static int callBack(void *data, int argc, char **argv, char **azColName){
    int i;
-   fprintf(stderr, "What is this? %s: ", (const char*)data);
+   fprintf(stderr, "%s ", (const char*)data);
  
    for(i = 0; i<argc; i++){
 //      printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-	  printf("ARGV %d is %s\n", i, argv[i]);
-
 	  printf("%s\n", argv[i] ? argv[i] : "NULL");
    }
    
@@ -29,26 +34,24 @@ void openDatabase( ) {
 
    printf("Database is %s \n", GLOBAL_DB_FILE);
 
-   if( LOCAL_RETURN_CODE_DB ) {
+   if( LOCAL_RETURN_CODE_DB != 0 ) {
       fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(GLOBAL_PTR_DATABASE));
-   } else {
-	  fprintf(stdout, "Opened database successfully\n");
+	  fprintf(stderr, "Exited with return code: %d\n", LOCAL_RETURN_CODE_DB);
    }
 }
 
 void runQuery() {
    int LOCAL_RETURN_CODE_QUERY;
    char *zErrMsg = 0;
-   const char* data = "Callback function called";
+   const char* data = " ";
 
    /* Execute SQL statement */
-   LOCAL_RETURN_CODE_QUERY = sqlite3_exec(GLOBAL_PTR_DATABASE, GLOBAL_SQL_QUERY, callback, (void*)data, &zErrMsg);
-   
-   if( LOCAL_RETURN_CODE_QUERY != SQLITE_OK ) {
+   LOCAL_RETURN_CODE_QUERY = sqlite3_exec(GLOBAL_PTR_DATABASE, GLOBAL_SQL_QUERY, callBack, (void*)data, &zErrMsg);
+
+   if( LOCAL_RETURN_CODE_QUERY != 0 ) {
       fprintf(stderr, "SQL error: %s\n", zErrMsg);
+	  fprintf(stderr, "Exited with return code: %d\n", LOCAL_RETURN_CODE_QUERY);
       sqlite3_free(zErrMsg);
-   } else {
-      fprintf(stdout, "Operation done successfully\n");
    }
 }
 
