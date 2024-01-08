a77c999629b11bee8f2ba6ca32849bc14fb2ffb5
neiltbz <z946a@hotmail.com>
2024-01-08 18:59:13 +0100

    2.0

--- 02_Code/src/create-word-database.c
+++ 02_Code/src/create-word-database.c
@@ -1,26 +1,26 @@
 //
 // Author: Neil Devlin
-// Version 1.0
+// Version 2.0
 //
 
 #include <stdio.h>
-#include <sqlite3.h> 
-#include <ctype.h> 
-#include <string.h> 
 #include <stdlib.h>
-
-
+#include <string.h> 
+#include <ctype.h> 
+#include <sqlite3.h> 
 
 // Global variables
 
 char *GLOBAL_ENV_VARIABLE_MIN="M431_MIN_WORD_LENGTH";
 char *GLOBAL_ENV_VARIABLE_MAX="M431_MAX_WORD_LENGTH";
 int GLOBAL_DEFAULT_WORD_LENGTH_MIN = 4;
-int GLOBAL_DEFAULT_WORD_LENGTH_MAX = 32;
-
+int GLOBAL_DEFAULT_WORD_LENGTH_MAX = 16;
+int GLOBAL_WORD_LENGTH_MIN;
+int GLOBAL_WORD_LENGTH_MAX;
 
 // Set some global variables
 sqlite3 *GLOBAL_PTR_DATABASE;
+FILE *GLOBAL_PTR_TEXT_FILE;
 
 // The location for the word file
 char *GLOBAL_DEFAULT_TEXT_FILE = "../db/deadly-insects.txt";
@@ -69,22 +69,22 @@ void setMeUp( ) {
 	LOCAL_ENV_VARIABLE_VALUE_MAX=getenv(GLOBAL_ENV_VARIABLE_MAX);
 
 	if ( LOCAL_ENV_VARIABLE_VALUE_MAX != NULL ) {
-		LOCAL_WORD_LENGTH_MAX=strtol(LOCAL_ENV_VARIABLE_VALUE_MAX, &somePtr1, 10);
+		GLOBAL_WORD_LENGTH_MAX=strtol(LOCAL_ENV_VARIABLE_VALUE_MAX, &somePtr1, 10);
     } else {
-        LOCAL_WORD_LENGTH_MAX = GLOBAL_DEFAULT_WORD_LENGTH_MAX;
+        GLOBAL_WORD_LENGTH_MAX = GLOBAL_DEFAULT_WORD_LENGTH_MAX;
     }
-	fprintf(stdout, "Maximum word length is set to: %d\n", LOCAL_WORD_LENGTH_MAX);
+	fprintf(stdout, "Maximum word length is set to: %d\n", GLOBAL_WORD_LENGTH_MAX);
 		
     // Get the value of the environment variable  (it will be a string, so we need to convert it to an int later)
 	LOCAL_ENV_VARIABLE_VALUE_MIN=getenv(GLOBAL_ENV_VARIABLE_MIN);
 
 	if ( LOCAL_ENV_VARIABLE_VALUE_MIN != NULL ) {
-		LOCAL_WORD_LENGTH_MIN=strtol(LOCAL_ENV_VARIABLE_VALUE_MIN, &somePtr2, 10);
+		GLOBAL_WORD_LENGTH_MIN=strtol(LOCAL_ENV_VARIABLE_VALUE_MIN, &somePtr2, 10);
     } else {
-        LOCAL_WORD_LENGTH_MIN = GLOBAL_DEFAULT_WORD_LENGTH_MIN;
+        GLOBAL_WORD_LENGTH_MIN = GLOBAL_DEFAULT_WORD_LENGTH_MIN;
     }
 	
-	fprintf(stdout, "Minimum word length is set to: %d\n", LOCAL_WORD_LENGTH_MIN);
+	fprintf(stdout, "Minimum word length is set to: %d\n", GLOBAL_WORD_LENGTH_MIN);
 
 	// Ask the user to supply the name of the database
 	fprintf(stdout, "Please enter the full pathname of the database file: ");
@@ -94,6 +94,7 @@ void setMeUp( ) {
     FILE *LOCAL_PTR_DATABASE_FILE=fopen(LOCAL_DATABASE_FILE, "r");
     if (LOCAL_PTR_DATABASE_FILE == NULL) {
 		fprintf(stdout, "Using database %s\n", LOCAL_DATABASE_FILE);
+		
 	} else {
 		fprintf(stderr, "ERROR: The database file %s already exists, please use a different name\n", LOCAL_DATABASE_FILE);
         exit(10);
@@ -109,8 +110,8 @@ void setMeUp( ) {
 	}
 
 	// try to access the text file, if not successfull then exit
-    FILE *LOCAL_PTR_TEXT_FILE=fopen(LOCAL_TEXT_FILE, "r");
-    if (LOCAL_PTR_TEXT_FILE != NULL) {
+    GLOBAL_PTR_TEXT_FILE = fopen(LOCAL_TEXT_FILE, "r");
+    if (GLOBAL_PTR_TEXT_FILE != NULL) {
 		fprintf(stdout, "Using text %s\n", LOCAL_TEXT_FILE);
 	} else {
 		fprintf(stderr, "ERROR: The text file %s already does not exist, please use a different name\n", LOCAL_TEXT_FILE);
@@ -126,6 +127,76 @@ void setMeUp( ) {
    }
 }	
 
+void readAndInsert( ) {
+    char LOCAL_LINE[255] = "";
+	char LOCAL_WORD_TO_INSERT[32];
+    char *LOCAL_END_OF_LINE;
+	
+	int LOCAL_LENGTH = 255;
+	int LOCAL_NUMBER_LINES_TOTAL = 1;
+	int LOCAL_NUMBER_LINES_VALID = 0;	
+	int LOCAL_COUNTER = 0;
+	int LOCAL_DISCARD_NOT_APLHA = 0;
+	int LOCAL_DISCARD_TOO_SHORT = 0;
+	int LOCAL_DISCARD_TOO_LONG = 0;
+	int LOCAL_TMP = 32;
+
+	
+    while (	fgets(LOCAL_LINE, 255, GLOBAL_PTR_TEXT_FILE) ) {
+
+       LOCAL_END_OF_LINE = "false";
+	   
+	   
+//	   fprintf(stdout, "Line = %d String = %s Length = %d\n", LOCAL_NUMBER_LINES_TOTAL, LOCAL_LINE, LOCAL_LENGTH);
+
+	   for ( LOCAL_COUNTER = 0; LOCAL_COUNTER < LOCAL_LENGTH; LOCAL_COUNTER++ ) {
+            if ( LOCAL_LINE[LOCAL_COUNTER] == '\n' ) {
+				if ( LOCAL_COUNTER >= GLOBAL_WORD_LENGTH_MIN ) {
+				   LOCAL_END_OF_LINE = "true";
+				}
+				else {
+					LOCAL_DISCARD_TOO_SHORT++;
+                }    
+				if ( LOCAL_COUNTER > GLOBAL_WORD_LENGTH_MAX ) {
+				   LOCAL_END_OF_LINE = "false";
+				   LOCAL_DISCARD_TOO_LONG++;
+                } 
+				LOCAL_COUNTER = 999;
+            }				
+		    else {
+			   if ( isalpha( LOCAL_LINE[LOCAL_COUNTER] ) != 0 ) {
+				   LOCAL_WORD_TO_INSERT[LOCAL_COUNTER] = tolower( LOCAL_LINE[LOCAL_COUNTER] );
+			   }
+			   else {
+			       LOCAL_DISCARD_NOT_APLHA++;
+				   LOCAL_COUNTER = 999;
+			   }
+			}
+	   }
+	
+	   if ( strcmp(LOCAL_END_OF_LINE,  "true") == 0 ) {
+
+		   fprintf(stdout, "Valid word: %s\n", LOCAL_WORD_TO_INSERT);
+		   LOCAL_NUMBER_LINES_VALID++;
+	   }
+	   
+       for ( LOCAL_COUNTER = 0; LOCAL_COUNTER < LOCAL_TMP; LOCAL_COUNTER++ ) {
+		   LOCAL_WORD_TO_INSERT[LOCAL_COUNTER] = '\0';
+	   }
+//			   
+	   LOCAL_NUMBER_LINES_TOTAL++;  
+	}
+	
+	fprintf(stdout, "SUMMARY: Lines processed            : %d\n", LOCAL_NUMBER_LINES_TOTAL);
+	fprintf(stdout, "SUMMARY: Lines discarded (not alpha): %d\n", LOCAL_DISCARD_NOT_APLHA);
+	fprintf(stdout, "SUMMARY: Lines discarded (too short): %d\n", LOCAL_DISCARD_TOO_SHORT);
+	fprintf(stdout, "SUMMARY: Lines discarded (too long) : %d\n", LOCAL_DISCARD_TOO_LONG);
+	
+	fprintf(stdout, "SUMMARY: Lines valid                : %d\n", LOCAL_NUMBER_LINES_VALID);
+	
+}	
+	
+
 //
 // Args: none
 // Return: none
@@ -133,5 +204,6 @@ void setMeUp( ) {
 void main( ) {
    
    setMeUp();
+   readAndInsert();
    
 }
